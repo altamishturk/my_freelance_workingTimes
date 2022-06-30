22117879701b656cd2abd95ea237c6836b6ce80f
altamish <altamishpasha@gmail.com>
2022-06-30 12:50:51 +0530

    bug fixes

--- connection/db_connection.js
+++ connection/db_connection.js
@@ -3,16 +3,12 @@ const mongoose = require('mongoose');
 
 const connect = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.URL);
+        await mongoose.connect(process.env.URL);
         
         console.log('connected to the database successfully');
     } 
     catch (error) {
-        res.json({
-            succes: false,
-            message: 'someting went wrong while connecting to the database',
-            error
-        })
+        onsole.log('could not connect to the database');
     }
 }
 
